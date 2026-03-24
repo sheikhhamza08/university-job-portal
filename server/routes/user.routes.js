0748bdd5919d8225bedd8bc4d0574608ec802012
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-03-24 07:50:10 -0700

    add multer middleware for file upload handling and call user inregiter route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,9 @@
 import express from "express";
-import {
-  register,
-} from "../controllers/user.controllers.js";
+import { register } from "../controllers/user.controllers.js";
+import { singleUpload } from "../middleware/multer.js";
 
 const userRouter = express.Router();
 
-userRouter.post("/register",  register);
+userRouter.post("/register", singleUpload, register);
 
 export default userRouter;
