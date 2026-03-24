61a7c8ccc53cc99246cffedce4ccbeebcf5ec7a2
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-03-24 08:43:14 -0700

    add logout  route to user router

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { login, register } from "../controllers/user.controllers.js";
+import { login, logout, register } from "../controllers/user.controllers.js";
 import { singleUpload } from "../middleware/multer.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", singleUpload, register);
 userRouter.post("/login", login);
+userRouter.post("/logout", logout);
 
 export default userRouter;
