4510d4ad60805a8809a72e4521672be1991a0d06
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-03-24 07:52:53 -0700

    implement user login controller with JWT authentication

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -55,3 +55,67 @@ export const register = async (req, res) => {
     console.log(error);
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+
+    if (!email || !password || !role) {
+      return res.status(400).json({
+        message: "Email or password is missing",
+        success: false,
+      });
+    }
+
+    let user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found",
+        success: false,
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    // check role is correct or not and also verify password
+    if (!isPasswordMatch || role != user.role) {
+      return res.status(400).json({
+        message: "Incorrect Credentials",
+        success: false,
+      });
+    }
+
+    const tokenData = {
+      userId: user._id,
+    };
+
+    const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    user = {
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      role: user.role,
+      profile: user.profile,
+    };
+
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+      })
+      .json({
+        message: `Welcome back ${user.fullname}`,
+        user,
+        success: true,
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
