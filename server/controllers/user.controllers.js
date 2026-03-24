c2b93d90750c0499b4e6ca4bca45c0776c4d0ee1
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-03-24 12:21:59 -0700

    implement user profile update controller with file upload support

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -130,3 +130,66 @@ export const logout = async (req, res) => {
     console.log(error);
   }
 };
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { fullname, email, phoneNumber, bio, skills } = req.body;
+    const file = req.file;
+
+    let cloudResponse = "";
+
+    // file upload
+    if (file) {
+      const fileUri = getDataUri(file);
+      cloudResponse = await cloudinary.uploader.upload(fileUri.content, {
+        resource_type: "auto",
+      });
+    }
+
+    let skillsArray;
+    if (skills) {
+      skillsArray = skills.split(",");
+    }
+
+    const userId = req.id;
+    let user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found",
+        success: false,
+      });
+    }
+
+    if (fullname) user.fullname = fullname;
+    if (email) user.email = email;
+    if (phoneNumber) user.phoneNumber = phoneNumber;
+    if (bio) user.profile.bio = bio;
+    if (skills) user.profile.skills = skillsArray;
+
+    // resume upload
+    if (file && cloudResponse) {
+      user.profile.resume = cloudResponse.secure_url;
+      user.profile.resumeOriginalName = file.originalname;
+    }
+
+    await user.save();
+
+    user = {
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      role: user.role,
+      profile: user.profile,
+    };
+
+    return res.status(200).json({
+      message: "Profile updated",
+      user,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
