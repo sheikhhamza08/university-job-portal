1f428117dab3871c44b6b7a7408be8d7668c6b9b
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-03-24 07:54:50 -0700

    add login route to user router

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { register } from "../controllers/user.controllers.js";
+import { login, register } from "../controllers/user.controllers.js";
 import { singleUpload } from "../middleware/multer.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", singleUpload, register);
+userRouter.post("/login", login);
 
 export default userRouter;
