5eb5b216d7269b257625ac4be1e06e3cad5e7173
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-03-24 12:23:18 -0700

    add update user profile route to user router

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { login, logout, register } from "../controllers/user.controllers.js";
+import {
+  login,
+  logout,
+  register,
+  updateProfile,
+} from "../controllers/user.controllers.js";
 import { singleUpload } from "../middleware/multer.js";
+import authUser from "../middleware/authUser.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", singleUpload, register);
 userRouter.post("/login", login);
 userRouter.post("/logout", logout);
+userRouter.post("/profile/update", authUser, singleUpload, updateProfile);
 
 export default userRouter;
