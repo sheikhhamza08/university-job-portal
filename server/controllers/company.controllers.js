d35f2e46c868eb8dfc052eb1e9597c62e9c688db
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-03-25 05:28:18 -0700

    implement company register  controller

---
+++ server/controllers/company.controllers.js
@@ -0,0 +1,38 @@
+import { Company } from "../models/company.models.js";
+
+export const registerCompany = async (req, res) => {
+  try {
+    const { companyName } = req.body;
+    if (!companyName) {
+      return res.status(400).json({
+        message: "Company name is required",
+        success: false,
+      });
+    }
+
+    let company = await Company.findOne({ companyName: companyName });
+    if (company) {
+      return res.status(400).json({
+        message: "Company already exists",
+        success: false,
+      });
+    }
+
+    company = await Company.create({
+      companyName: companyName,
+      userId: req.id,
+    });
+
+    return res.status(201).json({
+      message: "Company registered successfully",
+      company,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
