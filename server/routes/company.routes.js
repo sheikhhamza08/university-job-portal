96492df83ead5a3f11790624750497d894568843
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-03-25 06:16:13 -0700

    add get company detial route to company routes

--- server/routes/company.routes.js
+++ server/routes/company.routes.js
@@ -5,5 +5,6 @@ import authUser from "../middlewares/authUser.js";
 const companyRouter = express.Router();
 
 companyRouter.post("/register", authUser, registerCompany);
+companyRouter.get("/get", authUser, getCompanyDetailsByUser);
 
 export default companyRouter;
