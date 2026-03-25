936570a1a4839b7266afdcbc6ed6a24f8849e4a1
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-03-25 06:44:52 -0700

    implement get company detail by id controller

--- server/controllers/company.controllers.js
+++ server/controllers/company.controllers.js
@@ -58,3 +58,25 @@ export const getCompanyDetailsByUser = async (req, res) => {
     console.log(error);
   }
 };
+
+// get informations/details of a particular company
+export const getCompanyDetailsById = async (req, res) => {
+  try {
+    const companyId = req.params.id;
+    const company = await Company.findById(companyId);
+
+    if (!company) {
+      return res.status(404).json({
+        message: "No company found",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      company,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- server/routes/company.routes.js
+++ server/routes/company.routes.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { registerCompany } from "../controllers/company.controllers.js";
+import {
+  getCompanyDetailsByUser,
+  registerCompany,
+} from "../controllers/company.controllers.js";
 import authUser from "../middlewares/authUser.js";
 
 const companyRouter = express.Router();
