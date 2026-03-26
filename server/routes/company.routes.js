a5a6fa46141535d688198472abab6fd3f48a820c
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-03-26 06:02:26 -0700

    add update  company detial route to company routes

--- server/routes/company.routes.js
+++ server/routes/company.routes.js
@@ -5,11 +5,13 @@ import {
   registerCompany,
 } from "../controllers/company.controllers.js";
 import authUser from "../middlewares/authUser.js";
+import { singleUpload } from "../middleware/multer.js";
 
 const companyRouter = express.Router();
 
 companyRouter.post("/register", authUser, registerCompany);
 companyRouter.get("/get", authUser, getCompanyDetailsByUser);
 companyRouter.get("/get/:id", authUser, getCompanyDetailsById);
+companyRouter.post("/update/:id", authUser, singleUpload, updateCompany);
 
 export default companyRouter;
