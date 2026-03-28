0e8dad503831ceba4acad3b69aaefdbbcd1c699b
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-03-28 11:03:59 -0700

    add get all jobs  route

--- server/routes/job.routes.js
+++ server/routes/job.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { postJob } from "../controllers/job.controllers.js";
+import { getAllJobs, postJob } from "../controllers/job.controllers.js";
 import authUser from "../middlewares/authUser.js";
 
 const jobRouter = express.Router();
 
 jobRouter.post("/post", authUser, postJob);
+jobRouter.get("/get", getAllJobs);
 
 export default jobRouter;
