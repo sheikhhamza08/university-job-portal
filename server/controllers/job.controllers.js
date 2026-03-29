8551352ada9320d0e571c4e9d14892f0f9b0e314
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-03-29 12:25:15 -0700

    implement get job by id  controller for student role

--- server/controllers/job.controllers.js
+++ server/controllers/job.controllers.js
@@ -92,3 +92,28 @@ export const getAllJobs = async (req, res) => {
     console.log(error);
   }
 };
+
+// role === "student"
+export const getJobById = async (req, res) => {
+  try {
+    const jobId = req.params.id;
+
+    const job = await Job.findById(jobId).populate({
+      path: "applications",
+    });
+    if (!job) {
+      res.status(404).json({
+        message: "Job not found",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Job found",
+      job,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
