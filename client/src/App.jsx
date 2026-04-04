1e99eb93830a72b536e1b7dd5e3cc47856a07128
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-04-04 08:34:14 -0700

    Home route added for student role

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
 import Browse from "./components/Browse";
+import Home from "./components/Home";
 
 const appRouter = createBrowserRouter([
   // for student role
+  {
+    path: "/",
+    element: <Home />,
+  },
   {
     path: "/login",
     element: <Login />,
