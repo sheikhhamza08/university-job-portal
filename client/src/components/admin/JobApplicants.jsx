31b384c4ba610f885930c488553666e944cd0c20
sheikhhamza08 <sheikhhamzash01@gmail.com>
2026-04-05 10:01:11 -0700

    create job applicants component

--- client/src/components/admin/JobApplicants.jsx
+++ client/src/components/admin/JobApplicants.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../shared/Navbar";
 import JobApplicantsTable from "./JobApplicantsTable";
 import axios from "axios";
